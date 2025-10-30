2af7fbeecc5665db0c364a302263127f3053cc19
Maxence Isaac Castiau <misaac-c@c2r11s6.42madrid.com>
2025-10-30 18:46:28 +0100

    adding other test

--- includes/main.h
+++ includes/main.h
@@ -4,6 +4,7 @@
 // -- Library -- //
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/mman.h>
--- main.c
+++ main.c
@@ -8,3 +8,60 @@ int main(void)
         printf("nb -> %d\n", nb[i]);
     }
 }
+
+/*
+    printf("Test ft_malloc\n");
+
+    // --- Test tiny allocation ---
+    char *tiny1 = (char *)ft_malloc(16);
+    char *tiny2 = (char *)ft_malloc(32);
+
+    if (tiny1 && tiny2)
+    {
+        strcpy(tiny1, "Hello Tiny1!");
+        strcpy(tiny2, "Hello Tiny2!");
+        printf("tiny1: %s\n", tiny1);
+        printf("tiny2: %s\n", tiny2);
+    }
+    else
+        printf("Tiny allocation failed!\n");
+
+    // --- Test small allocation ---
+    char *small1 = (char *)ft_malloc(128);
+    char *small2 = (char *)ft_malloc(256);
+
+    if (small1 && small2)
+    {
+        strcpy(small1, "Hello Small1!");
+        strcpy(small2, "Hello Small2!");
+        printf("small1: %s\n", small1);
+        printf("small2: %s\n", small2);
+    }
+    else
+        printf("Small allocation failed!\n");
+
+    // --- Test large allocation ---
+    char *large1 = (char *)ft_malloc(1024);
+    if (large1)
+    {
+        strcpy(large1, "Hello Large!");
+        printf("large1: %s\n", large1);
+    }
+    else
+        printf("Large allocation failed!\n");
+
+    // --- Test multiple allocations to fill tiny zone ---
+    for (int i = 0; i < 10; i++)
+    {
+        char *p = (char *)ft_malloc(16);
+        if (p)
+        {
+            sprintf(p, "Tiny %d", i);
+            printf("%s\n", p);
+        }
+        else
+            printf("Allocation %d failed!\n", i);
+    }
+
+    return 0;
+*/
